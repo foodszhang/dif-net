f15f6e7de6f4071593ec2664083ec0bb18569206
foods zhang <foodszhang@gmail.com>
2024-11-21 18:47:12 +0800

    add

--- scripts/test.sh
+++ scripts/test.sh
@@ -2,7 +2,7 @@ gpu=0
 
 CUDA_VISIBLE_DEVICES=$gpu python code/evaluate.py \
     --name dif-net \
-    --epoch 400 \
-    --dst_list knee_zhao \
+    --epoch 2000 \
+    --dst_list knee_cbct \
     --split test \
     --num_views 10
